1b53c98d1fa1043703be9c388e4b4c27ad155d5b
Paul Rossotti <rossotti.paul@gmail.com>
2016-11-27 09:37:56 -0800

    remove TestContext

--- src/test/java/com/rossotti/services/TestContext.java
+++
@@ -1,9 +0,0 @@
-package com.rossotti.services;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("com.rossotti.services")
-public class TestContext {
-}
